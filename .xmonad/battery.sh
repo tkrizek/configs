aa0fb4eb33a2a5bfa8f57346407216a5a4121f8a
Tomas Krizek <tkrizek@redhat.com>
2016-09-20 15:51:25 +0200

    statusbar: fix battery charge display

--- .xmonad/battery.sh
+++ .xmonad/battery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PERCENT=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep '^    percentage:' | sed 's/[^0-9]*//' | sed s/%//`
 
-if upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep '^    state:' | grep ' charging'
+if [[ $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep '^    state:' | grep ' charging') ]];
 then
     if [ $PERCENT != 100 ]
     then
