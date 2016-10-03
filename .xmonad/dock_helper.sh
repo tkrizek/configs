06b6b3ed9386c292628363e37b792c56060bab7a
Tomas Krizek <tkrizek@redhat.com>
2016-10-03 15:17:05 +0200

    xmonad: dock: never a good idea to turn off the lcd screen :)

--- .xmonad/dock_helper.sh
+++ .xmonad/dock_helper.sh
@@ -10,7 +10,6 @@ xrandr --output ${LCD_SCREEN} --auto --primary >> /tmp/dock_helper.log 2>&1
 # Attempt to determine which external screens are attached
 xrandr | grep "${FIRST_EXTERNAL} connected" >> /tmp/dock_helper.log 2>&1
 if [ $? -eq 0 ]; then
-    xrandr --output ${LCD_SCREEN} -- off >> /tmp/dock_helper.log 2>&1
     xrandr --output ${FIRST_EXTERNAL} --auto --primary >> /tmp/dock_helper.log 2>&1
     # xrandr --output ${FIRST_EXTERNAL} --auto --primary --output ${LCD_SCREEN} --auto --left-of ${FIRST_EXTERNAL} >> /tmp/dock_helper.log 2>&1
 fi
